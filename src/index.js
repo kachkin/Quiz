fd922ac5f59cc9dd01733a76d56b0c284cc1b133
Alexander <kachkinsasha@gmail.com>
2017-07-31 17:41:44 +0300

    changed navigation

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ import {Provider} from "react-redux"
 import Questions from "./containers/Questions";
 import Themes from "./containers/Themes";
 import Result from "./containers/Result"
+//import LogInPage from "./containers/LogInPage"
 import configureStore from "./store/configureStore"
+
 const store = configureStore();
 render(
     <Provider store={store}>
         <Router history={browserHistory}>
-            <Route path='/' component={Themes}>
-                <Route path='themes/:theme' component={Questions}/>
-                <Route path='/result' component={Result}/>
-            </Route>
+                <Route path='/' component={Themes}>
+                    <Route path='themes/:theme' component={Questions}/>
+                    <Route path='result' component={Result}/>
+                </Route>
         </Router>
     </Provider>,
     document.getElementById("root")
