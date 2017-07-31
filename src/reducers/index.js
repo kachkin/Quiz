9af2b6d36f3515a0812f9c7f4d764243ff8bd39c
Alexander <kachkinsasha@gmail.com>
2017-07-31 17:33:22 +0300

    imported session reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from "redux";
 import questions from "./questions";
 import themes from "./themes";
+import session from"./session";
 
 export default combineReducers({
     questions,
-    themes
+    themes,
+    session
 });
