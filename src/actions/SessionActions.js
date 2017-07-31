ae8c936b4bc3d9d6836338f435984835fb0ac341
Alexander <kachkinsasha@gmail.com>
2017-07-31 16:45:48 +0300

    Created session actions

---
+++ src/actions/SessionActions.js
@@ -0,0 +1,55 @@
+import axios from "axios";
+
+import {
+    REGISTER_USER_ERROR,
+    REGISTER_USER_REQUEST,
+    REGISTER_USER_SUCCESS,
+    SESSION_AUTH_ERROR,
+    SESSION_AUTH_REQUEST,
+    SESSION_AUTH_SUCCESS
+} from "../constants/Session";
+
+export function registerUser(user) {
+    var url = "/api/register/";
+    return (dispatch) => {
+        dispatch({
+            type: REGISTER_USER_REQUEST
+        })
+        axios.post(url, user).then(
+            () => {
+                dispatch({
+                    type: REGISTER_USER_SUCCESS
+                })
+            }
+        ).catch((error) => {
+            dispatch({
+                type: REGISTER_USER_ERROR,
+                error: error
+            })
+        })
+    }
+}
+
+export function userAuth(login, password) {
+    var url = "/api/auth/"+login+"/" + password;
+
+    return (dispatch) => {
+        dispatch({
+            type: SESSION_AUTH_REQUEST,
+            payload:{}
+        });
+        axios.get(url).then(result => {
+            dispatch({
+                type: SESSION_AUTH_SUCCESS,
+                payload: {
+                    user: result
+                }
+            })
+        }).catch(error => {
+            dispatch({
+                type: SESSION_AUTH_ERROR,
+                error: error
+            })
+        })
+    }
+}
