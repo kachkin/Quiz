bf85ae6a92d4bf2e6104ae0fc66f41e5cafdffbf
Alexander <kachkinsasha@gmail.com>
2017-08-02 02:03:19 +0300

    changed return state

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -19,10 +19,10 @@ export default function session(state = initialState, action) {
         case SESSION_AUTH_SUCCESS:
             return {
                 ...state,
-                userFirstName: action.payload.user.firstName,
-                userLastName: action.payload.user.lastName,
-                login: action.payload.user.login,
-                teacher: action.payload.user.teacher
+                userFirstName: action.payload.user.data.firstName,
+                userLastName: action.payload.user.data.lastName,
+                login: action.payload.user.data.login,
+                teacher: action.payload.user.data.teacher
             }
         default:
             return state
