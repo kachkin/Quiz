0d86180f6a68f2503872d8a51617ede3e097a97e
Alexander <kachkinsasha@gmail.com>
2017-08-02 01:50:50 +0300

    Changed form authorization, added check empty inputs

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,39 +1,47 @@
-
 import React, {PropTypes, Component} from "react";
 
 import {Col, FormGroup, Form, FormControl, Button, ControlLabel} from "react-bootstrap"
 
-export default class LogIn extends Component{
-    handleFormSubmit(e){
-        e.preventDefault();
-        var login = e.target.elements["formHorizontalLogin"].value;
-        var password = e.target.elements["formHorizontalPassword"].value;
-        this.props.userAuth(login, password);
-    }
-    render(){
-        return<Form onSubmit={this.handleFormSubmit.bind(this)} horizontal>
+export default class LogIn extends Component {
+
+    render() {
+        const {userAuth, goToRegistration} = this.props
+        return <Form onSubmit={userAuth} horizontal>
             <FormGroup controlId='formHorizontalLogin'>
-                <Col componentClass={ControlLabel} sm={2}>
+                <Col componentClass={ControlLabel} sm={3}>
                     Login
                 </Col>
-                <Col sm={10}>
-                    <FormControl type='login' placeholder='Login' />
+                <Col sm={4}>
+                    <FormControl required type='text' placeholder='Login'/>
                 </Col>
             </FormGroup>
 
             <FormGroup controlId='formHorizontalPassword'>
-                <Col componentClass={ControlLabel} sm={2}>
+                <Col componentClass={ControlLabel} sm={3}>
                     Password
                 </Col>
-                <Col sm={10}>
-                    <FormControl type='password' placeholder='Password' />
+                <Col sm={4}>
+                    <FormControl required type='password' placeholder='Password'/>
+                </Col>
+            </FormGroup>
+
+            <FormGroup controlId='formErrorLogIn'>
+                <Col componentClass={ControlLabel} sm={3}>
+                </Col>
+                <Col id='error-login' sm={4}>
+
                 </Col>
             </FormGroup>
 
             <FormGroup>
-                <Col smOffset={2} sm={10}>
-                    <Button type='submit'>
-                        Sign in
+                <Col smOffset={3} sm={1}>
+                        <Button bsStyle='primary' type='submit'>
+                            Sign in
+                        </Button>
+                </Col>
+                <Col smOffset={1} sm={3}>
+                    <Button bsStyle='primary' type='button' onClick={goToRegistration}>
+                        Registration
                     </Button>
                 </Col>
             </FormGroup>
@@ -42,5 +50,6 @@ export default class LogIn extends Component{
 }
 
 LogIn.propTypes = {
-    userAuth: PropTypes.func.isRequired
+    userAuth: PropTypes.func.isRequired,
+    goToRegistration: PropTypes.func.isRequired
 }
