f732fbbdb10be4364a2d30e37d8f4ffa68aad450
Alexander <kachkinsasha@gmail.com>
2017-08-02 01:52:18 +0300

    Added diagram of results

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,3 +1,23 @@
-/**
- * Created by Администратор on 26.07.2017.
- */
+import React, {Component, PropTypes} from "react";
+import {RadialChart} from "react-vis";
+
+
+export default  class Result extends Component {
+    render() {
+        const { right, fail} = this.props;
+        return <div className='ResultPage'>
+            <RadialChart
+                width={300}
+                height={300}
+                data={[
+                    {angle: right}, {angle: fail}
+            ]}/>
+        </div>
+    }
+}
+
+Result.propTypes = {
+    theme: PropTypes.string.isRequired,
+    right: PropTypes.number.isRequired,
+    fail: PropTypes.number.isRequired
+};
