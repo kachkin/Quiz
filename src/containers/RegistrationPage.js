2074ff17564429a2c0bd65eb49f87faa17049072
Alexander <kachkinsasha@gmail.com>
2017-08-04 00:08:30 +0300

    Added methods handle validation of data

--- src/containers/RegistrationPage.js
+++ src/containers/RegistrationPage.js
@@ -1,7 +1,14 @@
 import React, {Component} from "react";
 import {bindActionCreators} from "redux";
 import {connect} from "react-redux";
-import {registerUser} from "../actions/SessionActions";
+import {
+    registerUser,
+    validateFirstName,
+    validateLastName,
+    validatePassword,
+    validateLogin,
+    checkRepeatPassword
+} from "../actions/SessionActions";
 import Registration from "../components/Registration"
 
 
@@ -9,6 +16,11 @@ class RegistrationPage extends Component {
 
     handleRegisterUser(e) {
         e.preventDefault();
+        if(this.props.validation.password&&
+            this.props.validation.firstName&&
+            this.props.validation.lastName&&
+            this.props.validation.login&&
+            this.props.validation.repeat){
         var firstName = e.target.elements["formFirstName"].value;
         var lastName = e.target.elements["formLastName"].value;
         var login = e.target.elements["formLogin"].value;
@@ -20,22 +32,59 @@ class RegistrationPage extends Component {
             password,
             teacher: false
         };
-        this.props.registerUser(user)
+        this.props.registerUser(user)}
+    }
+
+    handleValidateFirstName(value) {
+        this.props.validateFirstName(value);
+        return this.props.validation.firstName;
+    }
+
+    handleValidateLastName(value) {
+        this.props.validateLastName(value);
+        return this.props.validation.lastName;
+    }
+
+    handleValidatePassword(value){
+        this.props.validatePassword(value);
+        return this.props.validation.password;
+    }
+    handleValidateLogin(value){
+        this.props.validateLogin(value);
+        return this.props.validation.login;
+    }
+    handleCheckRepeatPassword(repeatPassword){
+        var password = document.getElementById("formPassword").value;
+        this.props.checkRepeatPassword(password, repeatPassword);
+        return this.props.validation.repeat;
     }
 
     render() {
         return (
-            <Registration registerUser={this.handleRegisterUser.bind(this)}/>
+            <Registration registerUser={this.handleRegisterUser.bind(this)}
+                          validateFirstName={this.handleValidateFirstName.bind(this)}
+                          validateLastName={this.handleValidateLastName.bind(this)}
+                          validatePassword={this.handleValidatePassword.bind(this)}
+                          validateLogin={this.handleValidateLogin.bind(this)}
+                          checkRepeatPassword={this.handleCheckRepeatPassword.bind(this)}
+            />
         )
     }
 }
 
-function State() {
-    return {}
+function State(state) {
+    return {
+        validation: state.session.validation
+    }
 }
 function Dispatch(dispatch) {
     return {
-        registerUser: bindActionCreators(registerUser, dispatch)
+        registerUser: bindActionCreators(registerUser, dispatch),
+        validateFirstName: bindActionCreators(validateFirstName, dispatch),
+        validateLastName: bindActionCreators(validateLastName, dispatch),
+        validatePassword: bindActionCreators(validatePassword, dispatch),
+        validateLogin: bindActionCreators(validateLogin, dispatch),
+        checkRepeatPassword: bindActionCreators(checkRepeatPassword, dispatch)
     }
 }
 
